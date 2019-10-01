cc8ff97f6dd326415d19ab55930e26460e124759
jacksonwb <jbeall@e1z3r1p1.42.us.org>
2019-10-01 12:08:04 -0700

    add L2 regularization and nesterov momentum

--- src/MultiLayerNet.py
+++ src/MultiLayerNet.py
@@ -3,12 +3,13 @@
 #  MultiLayerNet.py                                                            #
 #                                                                              #
 #  By - jacksonwb                                                              #
-#  Created: Wednesday December 1969 4:00:00 pm                                 #
-#  Modified: Monday Sep 2019 9:15:31 pm                                        #
+#  Created: Tuesday October 2019 11:43:06 am                                   #
+#  Modified: Tuesday Oct 2019 12:07:18 pm                                      #
 #  Modified By: jacksonwb                                                      #
 # ---------------------------------------------------------------------------- #
 
 import numpy as np
+import pickle
 
 class ConnectedLayer:
 	def __init__(self, input_n, size):
@@ -16,6 +17,7 @@ class ConnectedLayer:
 		self.size = size
 		self.w = 0.01 * np.random.randn(input_n, size)
 		self.b = 0.01 * np.ones([1, size])
+		self.v = 0
 	def forward(self, x):
 		return np.dot(x, self.w) + self.b
 	def backward(self, x, grad_out):
@@ -23,9 +25,18 @@ class ConnectedLayer:
 		self.dw = dw
 		self.db = np.sum(grad_out, axis=0, keepdims=True)
 		return np.dot(grad_out, self.w.T)
-	def update(self, eta):
+	def update(self, eta, reg):
+		self.dw += reg * self.w
 		self.w += -eta * self.dw
 		self.b += -eta * self.db
+		return reg * np.sum(self.w * self.w)
+	def update_nm(self, eta, reg, mu):
+		self.dw += reg * self.w
+		v_prev = self.v
+		self.v = mu * self.v - eta * self.dw
+		self.w += -mu * v_prev + (1 + mu) * self.v
+		self.b += -eta * self.db
+		return reg * np.sum(self.w * self.w)
 
 class ReLU:
 	def __init__(self, input_n, size):
@@ -36,7 +47,9 @@ class ReLU:
 	def backward(self, x, grad_out):
 		grad_out[x <= 0 ] = 0
 		return grad_out
-	def update(self, eta):
+	def update(self, eta, reg):
+		pass
+	def update_nm(self, eta, reg, mu):
 		pass
 
 class SoftmaxCrossEntropy:
@@ -78,62 +91,38 @@ class Network:
 			grad = layer.backward(x_local, grads[-1])
 			grads.append(grad)
 		return grads[::-1]
-	def train(self, x, y, eta, iterations, quiet=False, validate=False, validate_x=None, validate_y=None, decay=0):
+	def train(self, x, y, eta, iterations, reg=0, mu=0, quiet=False, validate=False, validate_x=None, validate_y=None, decay=0):
 		eta_val = eta
 		for i in range(iterations):
 			y_pred = self.forward(x)
 			grad = self.backward(y, y_pred)
+
+			# update and manage regularization
+			reg_loss = []
 			for layer in self.layers:
-				layer.update(eta_val)
-			self.loss.append(self.output_layer.cost(y, y_pred))
+				reg_loss_local = layer.update(eta_val, reg) if not mu else layer.update_nm(eta_val, reg, 0.1)
+				if reg_loss_local:
+					reg_loss.append(reg_loss_local)
+			reg_loss_term = np.sum(reg_loss) / len(reg_loss) if reg else 0
+			self.loss.append(self.output_layer.cost(y, y_pred) + reg_loss_term)
+
+			# validation and stats
 			if validate:
-				self.val_loss.append(self.output_layer.cost(validate_y, self.predict(validate_x)))
+				self.val_loss.append(self.output_layer.cost(validate_y, self.predict(validate_x)) + reg_loss_term)
 			if not quiet and not i % 1000:
 				print('epoch {}/{} - loss {}'.format(i, iterations, self.loss[-1]), end='')
 				if validate:
 					print(' - val_loss {}'.format(self.val_loss[-1]), end='')
 				print('\n', end='')
+
+			# learning rate annealing
 			if decay:
 				eta_val = eta * np.exp(-decay * i)
 	def predict(self, x):
 		return self.forward(x)
-
-if __name__ == '__main__':
-
-	import matplotlib.pyplot as plt
-
-	N = 100 # number of points per class
-	D = 2 # dimensionality
-	K = 3 # number of classes
-	X = np.zeros((N*K,D)) # data matrix (each row = single example)
-	y = np.zeros(N*K, dtype='uint8') # class labels
-	for j in range(K):
-		ix = range(N*j,N*(j+1))
-		r = np.linspace(0.0,1,N) # radius
-		t = np.linspace(j*4,(j+1)*4,N) + np.random.randn(N)*0.2 # theta
-		X[ix] = np.c_[r*np.sin(t), r*np.cos(t)]
-		y[ix] = j
-
-	net = Network()
-	net.add_layer('connected', 2, 100)
-	net.add_layer('ReLU', 100, 0)
-	net.add_layer('connected', 100, 100)
-	net.add_layer('ReLU', 100, 0)
-	net.add_layer('connected', 100, 3)
-	net.train(X, y, 0.2, 2000)
-
-	# plot the resulting classifier
-	h = 0.02
-	x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-	y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-	xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
-						np.arange(y_min, y_max, h))
-	x_data = np.c_[xx.ravel(), yy.ravel()]
-	Z = net.predict(x_data)
-	Z = np.argmax(Z, axis=1)
-	Z = Z.reshape(xx.shape)
-	plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral, alpha=0.8)
-	plt.scatter(X[:, 0], X[:, 1], c=y, s=20, cmap=plt.cm.Spectral, edgecolors='black')
-	plt.xlim(xx.min(), xx.max())
-	plt.ylim(yy.min(), yy.max())
-	plt.show()
+	# def save_model(self, model_name='MLN_model.pkl'):
+	# 	with open(model_name, mode='wb') as model_file:
+	# 		pickle.dump(self.layer, model_file)
+	# def load_model(self, model_name='MLN_model.pkl'):
+	# 	with open(model_name, 'rb') as model_file:
+	# 		self.layers = pickle.load(model_file)
